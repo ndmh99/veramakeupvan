2ad50a40304203806768b6e3441c513a14b42000
ndmh99 <minhhieuc501@gmail.com>
2026-05-25 20:18:24 -0700

    refactor: replace @astrojs/tailwind integration with manual PostCSS and Tailwind configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://veramakeupvan.com',
-  integrations: [sitemap(), tailwind()],
+  integrations: [sitemap()],
 });
